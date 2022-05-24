3ff7a2b04e9adf7bd3695a9f25228d168d288d40
DESKTOP-TR45LU3\P <solot.ja@inet.co.th>
2022-05-24 21:40:19 +0700

    update

---
+++ src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'google-map';
+
+  center: google.maps.LatLngLiteral | any
+  center2: [] | any = [13.748374670633623, 100.56731667350648]
+
+  markers: [] | any = {
+    lat: 13.748374670633623,
+    lng: 100.56731667350648
+  };
+  label: string = '';
+  titleP: string = 'Marker title ';
+  image: string | any = "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
+  // options = { animation: google.maps.Animation.BOUNCE }
+  options = { icon: this.image}
+
+  ngOnInit() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.center = {
+        lat: 13.748374670633623,
+        lng: 100.56731667350648,
+      }
+    })
+  }
+
+
+  addMarker() {
+    this.markers = {
+      position: {
+        lat: 13.748374670633623,
+        lng: 100.56731667350648,
+      },
+      label: {
+        color: 'red',
+        text: 'Marker label ',
+      },
+      title: 'Marker title ',
+      options: { animation: google.maps.Animation.BOUNCE },
+    }
+  }
+
+
+}
